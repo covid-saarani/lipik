dcb261584cc6984b643aaeef1f7080f3eb1e9288
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2022-04-17 00:28:29 +0530

    Fix: String with just spaces was causing conversion to int to fail.

--- Cases/mohfw.py
+++ Cases/mohfw.py
@@ -21,13 +21,24 @@
 
 
 # Import standard library dependencies.
-from typing import Any
+from typing import Any, Union
 
 # Import external dependencies.
 import pendulum
 
 # Import helper function.
 from Helpers.fuzzy_find_name import find_name
+
+
+def convert_to_int(n: Union[int, str]) -> int:
+    """Convert to int if not already."""
+    if isinstance(n, int):
+        return n
+    elif isinstance(n, str):
+        return int(n.strip() or 0)
+    else:
+        raise ValueError("Unsupported type.")
+# End of convert_to_int().
 
 
 def parse_mohfw(
@@ -49,8 +60,8 @@ def parse_mohfw(
         else:
             state = find_name(data["state_name"], pretty_states_tuple)
 
-        if reconciled := data["death_reconsille"]:  # If not an empty string.
-            pretty[state]["deaths"]["reconciled"] = int(reconciled)
+        pretty[state]["deaths"]["reconciled"] = convert_to_int(
+                                                    data["death_reconsille"])
 
         if reconciliation_only:
             continue
@@ -61,8 +72,8 @@ def parse_mohfw(
         # Confirmed cases:
 
         state_confirmed = pretty[state]["confirmed"]
-        confirmed = int(data["new_positive"] or 0)
-        prev_confirmed = int(data["positive"] or 0)
+        confirmed = convert_to_int(data["new_positive"])
+        prev_confirmed = convert_to_int(data["positive"])
         delta_confirmed = confirmed - prev_confirmed
 
         state_confirmed["current"] = confirmed
@@ -72,8 +83,8 @@ def parse_mohfw(
         # Active cases:
 
         state_active = pretty[state]["active"]
-        active = int(data["new_active"] or 0)
-        prev_active = int(data["active"] or 0)
+        active = convert_to_int(data["new_active"])
+        prev_active = convert_to_int(data["active"])
         delta_active = active - prev_active
 
         state_active["current"] = active
@@ -84,8 +95,8 @@ def parse_mohfw(
         # Recovered cases:
 
         state_recovered = pretty[state]["recovered"]
-        recovered = int(data["new_cured"] or 0)
-        prev_recovered = int(data["cured"] or 0)
+        recovered = convert_to_int(data["new_cured"])
+        prev_recovered = convert_to_int(data["cured"])
         delta_recovered = recovered - prev_recovered
 
         state_recovered["current"] = recovered
@@ -96,9 +107,9 @@ def parse_mohfw(
         # Deaths:
 
         state_deaths = pretty[state]["deaths"]
-        deaths = int(data["new_death"] or 0)
-        prev_deaths = int(data["death"] or 0)
-        delta_deaths = int(data["total"] or 0)
+        deaths = convert_to_int(data["new_death"])
+        prev_deaths = convert_to_int(data["death"])
+        delta_deaths = convert_to_int(data["total"])
 
         state_deaths["current"] = deaths
         state_deaths["previous"] = prev_deaths
